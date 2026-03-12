97815ef48347d696324bd82f4cbe4f88f060d732
chittrapriya5796 <krcpriya@gmail.com>
2026-03-12 17:07:41 +0530

    fs-read

--- fs.js
+++ fs.js
@@ -1,9 +1,9 @@
 const fs = require ('fs');
 //To append
-fs.appendFile('./logs/log.txt',"\nWelcome", (error) => {
+fs.readFile('./logs/log.txt',"utf-8", (error,data) => {
     if(error) {
-        console.log('Error append data to the file', error.message);
+        console.log('Error Reading data to the file', error.message);
         return
     }
-    console.log("Data append to the file")
+    console.log("Data reading to the file")
 })
