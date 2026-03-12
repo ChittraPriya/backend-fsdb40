e3d2a078cd94c2caa14a98e7e0e46d37b9c1e14b
chittrapriya5796 <krcpriya@gmail.com>
2026-03-12 16:41:51 +0530

    add nodemon

--- server.js
+++ server.js
@@ -4,36 +4,10 @@ const http = require ('http');
 
 const server = http.createServer((req,res) =>{
     //output as json format
-    // res.writeHead(200,{
-    //     "Content-type" : "application/json"
-    // })
-    //  res.end(JSON.stringify({ message: 'Hello NodeJS' }));
-
-
-    //output as text
-    //  res.writeHead(200,{
-    //       "Content-type" : "text/plain"
-    //  })
-
-    //  res.end('Hello NodeJS');
-
-    //output as html
-    res.writeHead(200, {
-        'Content-type' : "text/html"
-    })
-
-    res.end(`
-        <!doctye html>
-        <html>
-        <head>
-        <title> Test Nodejs API Server
-        </title>
-        </head>
-        <body>
-        <h1>Hello NodeJs</h1>
-        </body>
-        </html>
-        `);
+     res.writeHead(200,{
+         "Content-type" : "application/json"
+     })
+     res.end(JSON.stringify({ message: 'Hello NodeJS' }));
 });
 //start server
 server.listen(3001, "127.0.0.1", ()=>{
