fc6016d734274a9624a0ea019ecb7cf66b3f4335
chittrapriya5796 <krcpriya@gmail.com>
2026-03-12 16:35:20 +0530

    response receive as diff formats json,text,html

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const http = require ('http');
 
 //create server
+
 const server = http.createServer((req,res) =>{
-    res.end('Response of NodeJs is Successfull');
-})
+    //output as json format
+    // res.writeHead(200,{
+    //     "Content-type" : "application/json"
+    // })
+    //  res.end(JSON.stringify({ message: 'Hello NodeJS' }));
+
+
+    //output as text
+    //  res.writeHead(200,{
+    //       "Content-type" : "text/plain"
+    //  })
+
+    //  res.end('Hello NodeJS');
+
+    //output as html
+    res.writeHead(200, {
+        'Content-type' : "text/html"
+    })
+
+    res.end(`
+        <!doctye html>
+        <html>
+        <head>
+        <title> Test Nodejs API Server
+        </title>
+        </head>
+        <body>
+        <h1>Hello NodeJs</h1>
+        </body>
+        </html>
+        `);
+});
 //start server
 server.listen(3001, "127.0.0.1", ()=>{
     console.log("Server is connected at http://127.0.0.1:3001 ...")
