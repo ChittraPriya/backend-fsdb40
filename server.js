6a39f4b763b4156b09e6dac7d59d2320d56edf24
chittrapriya5796 <krcpriya@gmail.com>
2026-03-13 07:06:09 +0530

    mongodb connect and .env

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const app = require('./app.js')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-//start the express application
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log('Connected to mongodb database')
+    //start the express application
 app.listen(3000,(error)=>{
     if(error){
         console.log("Error Starting the server",error.message)
         return;
     }
     console.log("server is listenting at http://localhost:3000 ....")
+})
+})
+.catch((error) => {
+    console.log('Error connecting to mongodb database...', error.message)
 })
