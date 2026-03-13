f22e5f10edd52944eabb1451425a35f62e2e01bc
chittrapriya5796 <krcpriya@gmail.com>
2026-03-13 05:32:15 +0530

    added todos array data with in the express

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const todos = [
+      {
+    "createdAt": "2026-02-18T06:07:03.667Z",
+    "title": "Alberta Zemlak",
+    "description": "Innovative Computer featuring negligible technology and Cotton construction",
+    "isCompleted": false,
+    "id": "5"
+  },
+  {
+    "createdAt": "2026-02-22T14:38:23.993Z",
+    "title": "Meeting at 8PM Today",
+    "description": "Meeting on the register issues",
+    "isCompleted": true,
+    "id": "7"
+  },
+  {
+    "createdAt": "2026-02-22T00:29:15.689Z",
+    "title": "Buy Fruits",
+    "description": "Fruits..\n1.Apple\n2.Banana\n3.Papaya\n4.gauva",
+    "isCompleted": true,
+    "id": "8"
+  }
+]
+
 //Import Third party library - Nodejs framework = Express
 const express = require('express');
 
@@ -5,8 +29,9 @@ const express = require('express');
 const app = express();
 
 //configure route to the application
-app.get("/test",(req,res) =>{
-    res.json({message: "Hello World"})
+app.get("/todos",(req,res) =>{
+    res.json(todos)
 })
+
 
 module.exports = app;
