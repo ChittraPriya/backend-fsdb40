8126e670c018fc0c850a847165043b0a13e82353
chittrapriya5796 <krcpriya@gmail.com>
2026-03-13 05:51:03 +0530

    Router setup

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const todos = [
-      {
-    "createdAt": "2026-02-18T06:07:03.667Z",
-    "title": "Alberta Zemlak",
-    "description": "Innovative Computer featuring negligible technology and Cotton construction",
-    "isCompleted": false,
-    "id": "5"
-  },
-  {
-    "createdAt": "2026-02-22T14:38:23.993Z",
-    "title": "Meeting at 8PM Today",
-    "description": "Meeting on the register issues",
-    "isCompleted": true,
-    "id": "7"
-  },
-  {
-    "createdAt": "2026-02-22T00:29:15.689Z",
-    "title": "Buy Fruits",
-    "description": "Fruits..\n1.Apple\n2.Banana\n3.Papaya\n4.gauva",
-    "isCompleted": true,
-    "id": "8"
-  }
-]
 
 //Import Third party library - Nodejs framework = Express
 const express = require('express');
+const todoRouter = require('./routes/todoRoutes.js')
 
 //create an express app
 const app = express();
 
 //configure route to the application
-app.get("/todos",(req,res) =>{
-    res.json(todos)
-})
+app.use("/todos", todoRouter)
 
 
 module.exports = app;
---
+++ routes/todoRoutes.js
@@ -0,0 +1,35 @@
+const todos = [
+      {
+    "createdAt": "2026-02-18T06:07:03.667Z",
+    "title": "Alberta Zemlak",
+    "description": "Innovative Computer featuring negligible technology and Cotton construction",
+    "isCompleted": false,
+    "id": "5"
+  },
+  {
+    "createdAt": "2026-02-22T14:38:23.993Z",
+    "title": "Meeting at 8PM Today",
+    "description": "Meeting on the register issues",
+    "isCompleted": true,
+    "id": "7"
+  },
+  {
+    "createdAt": "2026-02-22T00:29:15.689Z",
+    "title": "Buy Fruits",
+    "description": "Fruits..\n1.Apple\n2.Banana\n3.Papaya\n4.gauva",
+    "isCompleted": true,
+    "id": "8"
+  }
+]
+//import express
+const express = require("express")
+
+//set up a router=> express
+const todoRouter = express.Router();
+
+//setup the routes using the router
+todoRouter.get("/",(req,res) =>{
+    res.json(todos)
+})
+//export the router
+module.exports = todoRouter;
