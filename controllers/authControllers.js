df98bdd446e6ab31ef63bca33cb32148c7884279
chittrapriya5796 <krcpriya@gmail.com>
2026-03-14 17:00:06 +0530

    Login Endpoint done

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -33,7 +33,31 @@ const authController = {
     } catch (error) {
         return res.status(500),json({message: `User registration failed: ${error.message}` })
     }
-}
+},
+    loginUser : async(req,res) => {
+        try {
+            //get the details from the request
+            const {email, password} = req.body
+            //get the user with the emailfrom the database
+            const user = await User.find({email});
+            //check if the user is already registered
+            if(user.length == 0){
+                //no such user
+                return res.status(500).json({message: "User not Registered"})
+            }
+            //check password match
+            const isPasswordValid = await bcrypt.compare(password, user[0].password);
+            if(!isPasswordValid){
+                //password incorrect
+                return res.status(400).json({message: "Password Incorrect"})
+            }
+            //both user already exists
+            //password correct
+            return res.status(200).json({message: "Login Successfully"})
+        } catch (error) {
+             return res.status(500),json({message: `User login failed: ${error.message}` })
+        }
+    }
 }
 
 module.exports = authController
